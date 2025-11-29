19e937c9c54ea9647ffda98f7440875aaf8faaec
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2025-11-29 20:32:20 +0530

    add database integration and logs endpoint

--- api/main.go
+++ api/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"encoding/json"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	_ "github.com/lib/pq"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -17,12 +20,20 @@ type LogPayload struct {
 	Message string `json:"message"`
 }
 
+var db *sql.DB
+
 func main() {
+	db, _ = sql.Open(
+		"postgres",
+		"host=localhost port=5432 user=postgres password=postgres dbname=sentinel sslmode=disable",
+	)
+
 	rdb = redis.NewClient(&redis.Options{
 		Addr: "localhost:6379",
 	})
 
 	app := fiber.New()
+	app.Use(cors.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Sentinel API Connected to Redis")
@@ -49,6 +60,46 @@ func main() {
 		}
 
 		return c.SendString("Log pushed to Redis")
+	})
+
+	app.Get("/logs", func(c *fiber.Ctx) error {
+		rows, err := db.Query(`
+		SELECT l.id, l.service, l.level, l.message, i.summary, l.timestamp
+		FROM logs l
+		LEFT JOIN log_insights i ON l.id = i.log_id
+		ORDER BY l.id DESC
+		LIMIT 20
+	`)
+		if err != nil {
+			return c.Status(500).SendString("DB error")
+		}
+		defer rows.Close()
+
+		type LogView struct {
+			ID        int    `json:"id"`
+			Service   string `json:"service"`
+			Level     string `json:"level"`
+			Message   string `json:"message"`
+			Summary   string `json:"summary"`
+			Timestamp string `json:"timestamp"`
+		}
+
+		var result []LogView
+
+		for rows.Next() {
+			var log LogView
+			rows.Scan(
+				&log.ID,
+				&log.Service,
+				&log.Level,
+				&log.Message,
+				&log.Summary,
+				&log.Timestamp,
+			)
+			result = append(result, log)
+		}
+
+		return c.JSON(result)
 	})
 
 	app.Listen(":3000")
